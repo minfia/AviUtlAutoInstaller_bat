ba86d6d4f6566ab475da2fdb5db16e794b5c8acc
min <mintofia@gmail.com>
2019-04-28 21:26:27 +0900

    ダウンロードエラー時に再試行をするように変更

--- AviUtlAutoInstaller.bat
+++ AviUtlAutoInstaller.bat
@@ -26,6 +26,8 @@ setlocal ENABLEDELAYEDEXPANSION
 
 title AviUtl Auto Installer
 
+set DL_RETRY=3
+
 echo script version 1.5.0
 echo これはAviUtlの環境を構築するプログラムです。
 echo また、劇場向けの構成となります。
@@ -136,7 +138,7 @@ set WGETEXE="%WGET_DIR%\wget.exe"
 call :FILE_DOWNLOAD http://spring-fragrance.mints.ne.jp/aviutl/%AVIUTL_ZIP% "%DL_DIR%\%AVIUTL_ZIP%"
 call :FILE_DOWNLOAD http://spring-fragrance.mints.ne.jp/aviutl/%EXEDIT_ZIP%  "%DL_DIR%\%EXEDIT_ZIP%"
 call :FILE_DOWNLOAD https://pop.4-bit.jp/bin/l-smash/%LSMASH_ZIP% "%DL_DIR%\%LSMASH_ZIP%"
-call :FILE_DOWNLOAD "https://drive.google.com/uc?id=1fp6i-suNAlwCLsjXovJ-xXuUlNQmMQXK" "%DL_DIR%\%X264GUIEX_ZIP%"
+call :FILE_DOWNLOAD https://drive.google.com/uc?id=1fp6i-suNAlwCLsjXovJ-xXuUlNQmMQXK "%DL_DIR%\%X264GUIEX_ZIP%"
 
 
 @rem AviUtlの展開
@@ -312,12 +314,20 @@ exit /b
 @rem ファイルをダウンロードする
 @rem 引数: %1-URL %2-ダウンロードしたファイル名
 :FILE_DOWNLOAD 
-%WGETEXE% --no-check-certificate %1 -O %2
-    if %ERRORLEVEL% neq 0 (
-        call :SHOW_MSG "ファイルのダウンロードに失敗しました" vbCritical "エラー"
-        rmdir /s /q "%AVIUTL_DIR%"
-        exit
+    for /l %%a in (0,1,%DL_RETRY%) do (
+        if %%a gtr 0 (
+            echo Retry %%a/%DL_RETRY%
+        )
+        %WGETEXE% --no-check-certificate %1 -O %2
+        if !ERRORLEVEL! equ 0  (
+            goto :DOWNLOAD_SUCCESS
+        )
+        echo retVal:!ERRORLEVEL!
     )
+    call :SHOW_MSG "ファイルのダウンロードに失敗しました" vbCritical "エラー"
+    rmdir /s /q "%AVIUTL_DIR%"
+    exit
+:DOWNLOAD_SUCCESS
 exit /b
 
 @rem メッセージボックスを表示する
